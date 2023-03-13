1af45bd2a44b4579ce145b7e9bb55bbab502a28a
RitwikSingh28 <itsritwik26@gmail.com>
2023-03-13 15:11:40 +0530

    create product_list ui

--- lib/screens/productList.dart
+++ lib/screens/productList.dart
@@ -38,14 +38,81 @@ class _ProductListState extends State<ProductList> {
               itemCount: products['productName']?.length,
               itemBuilder: (context, index) {
                 return Card(
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Text(index.toString()),
-                        ],
-                      ),
-                    ],
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Image(
+                              height: 100,
+                              width: 100,
+                              image: NetworkImage(
+                                products['productImage']?[index],
+                              ),
+                              fit: BoxFit.contain,
+                            ),
+                            const SizedBox(width: 15),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  products['productName']?[index],
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      products['productUnit']?[index],
+                                      style: const TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                    ),
+                                    Text(
+                                      ' | Rs. ${products['productPrice']?[index]}',
+                                      style: const TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Container(
+                                  height: 35,
+                                  width: 100,
+                                  decoration: const BoxDecoration(
+                                    color: Colors.lightGreen,
+                                  ),
+                                  child: const Center(
+                                    child: Text(
+                                      'Add to Cart',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 );
               },
