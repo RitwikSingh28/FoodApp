434e663c4cd7171da639fdf09b013d6a98113cda
RitwikSingh28 <itsritwik26@gmail.com>
2023-03-13 14:42:16 +0530

    add dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,11 @@
 import 'package:flutter/material.dart';
+import './screens/productList.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       title: 'Food Basket',
-      home: HomePage(),
+      home: ProductList(),
     ),
   );
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Food Items'),
-      ),
-      body: Container(),
-    );
-  }
 }
