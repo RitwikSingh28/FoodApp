15b5221b8a0160d6095780719cbb0e3151b4efe2
RitwikSingh28 <itsritwik26@gmail.com>
2023-03-14 09:32:16 +0530

    add db specs

---
+++ lib/repository/database.dart
@@ -0,0 +1,42 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io' as io;
+import 'package:path/path.dart';
+import './../model/cart_model.dart';
+
+class DBHelper {
+  static Database? _db;
+
+  Future<Database?> get db async {
+    if (_db != null) {
+      return _db;
+    }
+
+    _db = await initDatabase();
+  }
+
+  initDatabase() async {
+    io.Directory documentDirectory = await getApplicationDocumentsDirectory();
+
+    String path = join(documentDirectory.path, 'cart.db');
+    var db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute(
+          'CREATE TABLE cart (id INTEGER PRIMARY KEY, productId VARCHAR UNIQUE, productName TEXT, initialPrice INTEGER, productPrice INTEGER , qty INTEGER, unitTag TEXT , imgUrl TEXT);',
+        );
+      },
+    );
+    return db;
+  }
+
+  Future<CartModel?> insert(CartModel cart) async {
+    var dbClient = await db;
+    await dbClient?.insert(
+      'cart',
+      cart.toJSON(),
+    );
+    return cart;
+  }
+}
